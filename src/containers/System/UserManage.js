b2c672becc15e0bc6a4b4ef756baf43938b94a47
DONAM-IT <doducnam.it@gmail.com>
2023-03-21 23:28:47 +0700

    fifth commit

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,20 +3,17 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
 import { getAllUsers } from "../../services/userService";
+import ModalUser from "./ModalUser";
+
 class UserManage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrUser: [],
+      isOpenModalUser: false,
     };
   }
-  /** Life cycle
-   *  Run component:
-   * 1. Run construct -> init state //tức là khai báo các biến muốn dùng
-   * 2. Did mount(set state) muốn gán giá trị cho 1 biến state nào đấy trước khi render ra màn hình
-   * 3. Render
-   *
-   */
+
   async componentDidMount() {
     //gọi API - để lấy dữ liệu từ api về
     let response = await getAllUsers("ALL");
@@ -42,12 +39,44 @@ class UserManage extends Component {
     // console.log("get user from node.js :", response);
   }
 
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser: true,
+    });
+    // alert("click me");
+  };
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModalUser: !this.state.isOpenModalUser,
+    });
+  };
+  /** Life cycle
+   *  Run component:
+   * 1. Run construct -> init state //tức là khai báo các biến muốn dùng
+   * 2. Did mount(set state): born >< unmount muốn gán giá trị cho 1 biến state nào đấy trước khi render ra màn hình
+   * 3. Render (re-render)
+   *
+   */
   render() {
-    // console.log("check render", this.state);
     let arrUser = this.state.arrUser;
+    console.log(arrUser);
+    //properties ; nested
     return (
       <div className="users-container">
+        <ModalUser
+          isOpen={this.state.isOpenModalUser}
+          toggleFromParent={this.toggleUserModal}
+          test={"abc"}
+        />
         <div className="title text-center">Manage users with Eric</div>
+        <div className="mx-1">
+          <button
+            className="btn btn-primary px-3"
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i className="fas fa-plus"></i> Add new users
+          </button>
+        </div>
         <div className="users-table mt-3 mx-1">
           <table id="customers">
             <tr>
@@ -60,7 +89,7 @@ class UserManage extends Component {
 
             {arrUser &&
               arrUser.map((item, index) => {
-                console.log("eric check map ", item, index);
+                // console.log("eric check map ", item, index);
                 return (
                   <tr key={index}>
                     <td>{item.email}</td>
