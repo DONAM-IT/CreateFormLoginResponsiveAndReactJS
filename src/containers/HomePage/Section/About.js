75db8825f26014b8729d95aa222871ea992da9d6
DONAM-IT <doducnam.it@gmail.com>
2023-03-29 22:49:25 +0700

    eighteenth commit

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -18,9 +18,9 @@ class About extends Component {
               height="400px"
               src="https://www.youtube.com/embed/oDyCAgjQ7gY"
               title="không phải dạng vừa đâu-Sơn Tùng M-TP"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
           <div className="content-right">
